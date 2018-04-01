fa229eb188b514e0230a332e727c25e419f48ec4
qinchy <qinchy@msn.com>
2018-04-01 21:06:37 +0800

    Initial commit

---
+++ hellomavenplugin/src/main/java/com/qinchy/HelloMojo.java
@@ -0,0 +1,70 @@
+package com.qinchy;
+
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Goal which touches a timestamp file.
+ *
+ * @goal touch
+ * @phase package
+ */
+@Mojo(name="hello")
+public class HelloMojo extends AbstractMojo {
+    /**
+     * Location of the file.
+     *
+     * @parameter property="${project.build.directory}"
+     * @required
+     */
+    private String outputDirectory;
+
+    public void execute()
+            throws MojoExecutionException {
+        File f = new File(outputDirectory);
+
+        if (!f.exists()) {
+            f.mkdirs();
+        }
+
+        File touch = new File(f, "touch.txt");
+
+        FileWriter w = null;
+        try {
+            w = new FileWriter(touch);
+
+            w.write("touch.txt");
+        } catch (IOException e) {
+            throw new MojoExecutionException("Error creating file " + touch, e);
+        } finally {
+            if (w != null) {
+                try {
+                    w.close();
+                } catch (IOException e) {
+                    // ignore
+                }
+            }
+        }
+    }
+}
---
+++ pluginusagedemo/src/main/java/com/qinchy/pluginusagedemo/PluginusagedemoApplication.java
@@ -0,0 +1,12 @@
+package com.qinchy.pluginusagedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PluginusagedemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PluginusagedemoApplication.class, args);
+    }
+}
